e927369cbdf204a93797a9e532de757c99a18f7b
Demi-Ad <votm777@naver.com>
2021-03-21 15:45:53 +0900

    출력 구현

--- include/db_access.py
+++ include/db_access.py
@@ -1,12 +1,20 @@
 import sqlite3
-from typing import List
+from typing import List, Generator, Optional
+from pprint import pprint
+from collections import OrderedDict
 
 
 class DbSetUp:
     def __init__(self, db_path: str):
-        self.conn = sqlite3.connect(db_path)
-        self.cursor = self.conn.cursor()
+        self._conn = sqlite3.connect(db_path)
+        self._cursor = self._conn.cursor()
 
+    def clear(self):
+        sql = "delete from InputData"
+        sql2 = "update sqlite_sequence set seq = 0 where name = 'InputData'"
+        self._cursor.execute(sql)
+        self._cursor.execute(sql2)
+        self._conn.commit()
 
 class DbWriter(DbSetUp):
     def __init__(self, db_path: str):
@@ -14,21 +22,90 @@ class DbWriter(DbSetUp):
 
     def insert(self, *args) -> None:
         sql = "insert into InputData(file_name, extension_name, data, created_at) values (?,?,?,?)"
-        self.cursor.execute(sql, args)
-        self.conn.commit()
+        self._cursor.execute(sql, args)
+        self._conn.commit()
 
     def __del__(self):
-        self.cursor.close()
+        self._cursor.close()
 
 
 class DbSelect(DbSetUp):
     def __init__(self, db_path: str):
         super().__init__(db_path)
 
-    def select(self, sql: str, *args) -> List:
-        self.cursor.execute(sql, args)
-        data = self.cursor.fetchall()
-        return data
+    def select(self, *args, extension: bool = False, file_name: bool = False):
+        if len(args) > 0:
+            if extension:
+                self.__extension_filter(args)
+            elif file_name:
+                self.__name_filter(args)
+            else:
+                self.__normal()
+        else:
+            self.__normal()
+
+        data = self._cursor.fetchall()
+        view_data = dict()
+        for index, col in enumerate(data):
+            view_data[col[0]] = {'file_name': col[1], 'extension': col[2], "created_at": col[3]}
+        pprint(view_data)
+
+    def __extension_filter(self, args):
+        sql = "select ID, file_name, extension_name, created_at from InputData where extension_name = (?)"
+        self._cursor.execute(sql, args)
+
+    def __name_filter(self, args):
+        sql = "select ID, file_name, extension_name, created_at from InputData where file_name LIKE '%' || (?) || '%'"
+        self._cursor.execute(sql, args)
+
+    def __normal(self):
+        sql = "select ID, file_name, extension_name, created_at from InputData"
+        self._cursor.execute(sql)
 
     def __del__(self):
-        self.cursor.close()
+        self._cursor.close()
+
+
+class DbOutPut(DbSetUp):
+    def __init__(self, db_path: str):
+        super().__init__(db_path)
+        self.__out_list = []
+
+    def extension_out(self, args):
+        sql = "select file_name, extension_name, data from InputData where extension_name = (?)"
+        loop = len(args)
+
+        for i in range(loop):
+            self._cursor.execute(sql, (args[i],))
+            self.__out_list.append(self._cursor.fetchall())
+
+        yield from self.__out_list
+
+    def file_name_out(self, arg):
+        sql = "select file_name, extension_name, data from InputData where file_name LIKE '%' || (?) || '%'"
+        self._cursor.execute(sql, arg)
+        self.__out_list.append(self._cursor.fetchall())
+
+        yield from self.__out_list
+
+    def index_out(self, *args):
+        sql = "select file_name, extension_name, data from InputData where ID between (?) and (?)"
+        self._cursor.execute(sql, args)
+        self.__out_list.append(self._cursor.fetchall())
+        yield from self.__out_list
+
+    def out(self, args):
+        sql = "select file_name, extension_name, data from InputData where id = (?)"
+        for i in args:
+            self._cursor.execute(sql, (int(i), ))
+            self.__out_list.append(self._cursor.fetchone())
+        yield from self.__out_list
+
+    def all_out(self):
+        sql = "select file_name, extension_name, data from InputData"
+        self._cursor.execute(sql)
+        data = self._cursor.fetchall()
+        yield from data
+
+    def __del__(self):
+        self._cursor.close()
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import sys
-import re
-from typing import List, Union, Set, Tuple
+"""
+created by demi
+"""
 import datetime
-from include.db_access import DbSelect, DbWriter
-from include.file_search import FileSearch
+import os.path
+import re
+import sys
+from sqlite3 import Binary
+from typing import List, Union
+
 from config import db_path
+from include.db_access import DbSelect, DbWriter, DbOutPut, DbSetUp
+from include.file_search import FileSearch
 
 
 def data2db(datas: List[str]):  # 데이터를 DB에 삽입
@@ -13,28 +19,66 @@ def data2db(datas: List[str]):  # 데이터를 DB에 삽입
         file = data.split('\\')[-1]
         file_name, file_extension = file.split('.')
         with open(data, 'rb') as blob:
-            blob_data = blob.readline()
-            DbWriter(db_path).insert(file_name, file_extension, blob_data, now)
+            blob_data = blob.read()
+            DbWriter(db_path).insert(file_name, file_extension, Binary(blob_data), now)
 
 
+def converter(data: List, out_path: str, flag: bool = False):
+    if flag:
+        for i in data:
+            with open(os.path.join(out_path, i[0] + '.' + i[1]), 'wb') as file:
+                file.write(i[2])
+    else:
+        for i in data:
+            for j in i:
+                with open(os.path.join(out_path, j[0] + '.' + j[1]), 'wb') as file:
+                    file.write(j[2])
 
-def db2data():
-    pass
+
+def db2data(out_path: str, trigger: Union[int, List[str]], sig: Union[List[str], str, int] = None):
+    d_out = DbOutPut(db_path)
+
+    if trigger == 1:
+        data = d_out.out(sig)
+        converter(data, out_path, True)
+
+    if trigger == 2:
+        data = d_out.extension_out(sig)
+        converter(data, out_path)
+
+    if trigger == 3:
+        data = d_out.file_name_out(sig)
+        converter(data, out_path)
+
+    if trigger == 4:
+        num1, num2 = map(int, sig[0].split('-'))
+        data = d_out.index_out(num1, num2)
+        converter(data, out_path)
+
+    if trigger == 5:
+        data = d_out.all_out()
+        converter(data, out_path, True)
 
 
-def choice_data(path: str, flag: bool = True) -> Union[List[str], str]:
+def choice_data(path: str, flag: bool) -> List[Union[int, str]]:
     data, extension = FileSearch(path, flag).out_data()
     temp = []
 
     for index, file in data:
         print(index, file)
         temp.append(file)
-    print(extension)
+
+    print('extension : ', end='')
+
+    for ext in extension:
+        print(ext, end=' ')
+
+    print()
 
     while True:
-        answer = input("인덱스 또는 확장자를 입력해주세요 : ")
-        if re.search('-', answer):  # 인덱스 선택
-            num1, num2 = map(int, answer.split('-'))
+        answer_1 = input("인덱스 또는 확장자를 입력해주세요 : ")
+        if re.search('-', answer_1):  # 인덱스 선택
+            num1, num2 = map(int, answer_1.split('-'))
             if num1 >= num2:
                 print("첫번째 인덱스는 두번째 인덱스보다 작아야합니다")
                 pass
@@ -42,46 +86,99 @@ def choice_data(path: str, flag: bool = True) -> Union[List[str], str]:
                 temp = temp[num1:num2 + 1]
                 return temp
 
-        elif re.match('[a-z]+', answer):  # 확장자 선택
-            if len(answer.split(', ')) == 1:
-                temp = [i for i in temp if i.split('.')[-1] == answer]
+        elif re.match('[a-z]+', answer_1):  # 확장자 선택
+            if len(answer_1.split(', ')) == 1:
+                temp = [i for i in temp if i.split('.')[-1] == answer_1]
                 return temp
             else:
                 data_temp = []
-                keyword = answer.split(', ')
+                keyword = answer_1.split(', ')
                 for file in temp:
                     for key in keyword:
                         if file.split('.')[-1] == key:
                             data_temp.append(file)
                 if len(data_temp) == 0:
                     print("올바른 확장자를 입력하세요")
-                    pass
+                    break
                 else:
                     return data_temp
 
-        elif re.match('[0-9]+', answer):  # 인덱스 단일 선택
-            return temp[int(answer)]
+        elif re.match('[0-9]+', answer_1):  # 인덱스 단일 선택
+            out_data = []
+            if len(answer_1) > 1:
+                choose = answer_1.split(' ')
+                for i in enumerate(temp):
+                    for j in choose:
+                        if i[0] == int(j):
+                            out_data.append(i[1])
 
-        elif answer == "":  # 전체 선택
+            else:
+                for i in enumerate(temp):
+                    if i[0] == int(answer_1):
+                        out_data.append(i[1])
+
+            return out_data
+
+        elif answer_1 == "":  # 전체 선택
             return temp
 
-        else:
-            print("지원하지 않는 형태입니다")
 
-
-def select_db():
-    pass
 
 
 def etc():
     pass
 
 
-def main():
+def main(search_range: bool = True):
     path = input('경로를 입력해주세요 : ')
-    data = choice_data(path)  # TODO : 명령줄 인자 받으면 input 안하기
+    if search_range:
+        data = choice_data(path, flag=True)
+    else:
+        data = choice_data(path, flag=False)
     data2db(data)
 
 
 if __name__ == '__main__':
-    main()
+    argv = sys.argv
+    if len(argv) >= 2:
+
+        if argv[1] == "--s":
+            main(search_range=True)
+        elif argv[1] == "--sd":
+            main(search_range=False)
+
+        elif argv[1] == "--help":
+            etc()
+
+        elif argv[1] == "--v":
+            if len(argv) >= 4:
+                if argv[2] == "--ext":
+                    DbSelect(db_path).select(argv[3], extension=True)
+                elif argv[2] == "--name":
+                    DbSelect(db_path).select(argv[3], file_name=True)
+                else:
+                    print("ERROR !!")
+            else:
+                DbSelect(db_path).select()
+
+        elif argv[1] == "--o":  # out
+            db2data(argv[2], sig=argv[3:], trigger=1)
+        elif argv[1] == "--oe":  # extension out
+            db2data(argv[2], sig=argv[3:], trigger=2)
+        elif argv[1] == "--of":  # filename out
+            db2data(argv[2], sig=argv[3:], trigger=3)
+        elif argv[1] == "--oi":  # index out
+            db2data(argv[2], sig=argv[3:], trigger=4)
+        elif argv[1] == "--oa":
+            db2data(argv[2], trigger=5)
+
+        elif argv[1] == "--clean":
+            while True:
+                answer = input("Are You Sure? y/n[n] : ")
+                if answer.upper() == "Y":
+                    DbSetUp(db_path).clear()
+                    break
+                else:
+                    break
+    else:
+        main()
